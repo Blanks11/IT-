a6bf10c68377a619a2a0720be0f9fd563b999a86
blanks <gabrieldivine08@gmail.com>
2026-08-17 23:49:16 +0100

    Create task5.js

---
+++ task5.js
@@ -0,0 +1,9 @@
+// 5. True or False Practice
+console.log("\n--- Task 5 ---");
+const votingAge = 18;
+const currentAge = 25;
+const isOldEnoughToVote = currentAge >= votingAge;
+const hasId = true;
+
+console.log(`Is old enough to vote? ${isOldEnoughToVote}`);
+console.log(`Can vote today? ${isOldEnoughToVote && hasId}`);
