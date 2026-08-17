df6ccd5871501646c35e08452cd69c1cde721ccc
blanks <gabrieldivine08@gmail.com>
2026-08-17 23:48:46 +0100

    Create task4.js

---
+++ task4.js
@@ -0,0 +1,8 @@
+// 4. Grade Average Calculator
+console.log("\n--- Task 4 ---");
+const score1 = 88;
+const score2 = 95;
+const score3 = 82;
+const average = (score1 + score2 + score3) / 3;
+
+console.log(`The average test score is ${average.toFixed(2)}`);
