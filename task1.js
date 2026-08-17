fb8942e31fbc88bcc14de9fdc6b5924b7f0bb759
blanks <gabrieldivine08@gmail.com>
2026-08-17 23:47:05 +0100

    Create task1.js

---
+++ task1.js
@@ -0,0 +1,8 @@
+// 1. Personal Info Program
+console.log("--- Task 1 ---");
+const myName = "Divine Amuh";
+const myAge = 25; 
+const myCity = "Port Harcourt";
+const myHobby = "developing Web3 apps and watching football";
+
+console.log(`My name is ${myName}, I am ${myAge} years old, I live in ${myCity}, and my favorite hobby is ${myHobby}.`);
