9ebf78ec1a8733347f90702894a0ccaa2fcf95e6
blanks <gabrieldivine08@gmail.com>
2026-08-17 23:48:17 +0100

    Create task3.js

---
+++ task3.js
@@ -0,0 +1,8 @@
+// 3. Rectangle Area & Perimeter Calculator
+console.log("\n--- Task 3 ---");
+const length = 15;
+const width = 10;
+const area = length * width;
+const perimeter = 2 * (length + width);
+
+console.log(`Rectangle Area: ${area}, Perimeter: ${perimeter}`);
