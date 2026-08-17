5449bba98630805d72cc9565985dacbbb74309ec
blanks <gabrieldivine08@gmail.com>
2026-08-17 23:47:44 +0100

    Create task2.js

---
+++ task2.js
@@ -0,0 +1,6 @@
+// 2. Temperature Converter
+console.log("\n--- Task 2 ---");
+const celsius = 28;
+const fahrenheit = (celsius * 9/5) + 32;
+
+console.log(`${celsius}°C is equal to ${fahrenheit}°F.`);
